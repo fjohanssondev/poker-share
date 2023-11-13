0026e2e7238af77259aeefaaf1a440745bb5a19d
Fredrik Johansson <fjohansson.creative@gmail.com>
2023-11-13 18:44:52 +0100

    feat: return all games that a user either created or participated in. Also created a procedure to create a game with all the users selected

--- src/server/api/routers/game.ts
+++ src/server/api/routers/game.ts
@@ -4,17 +4,42 @@ import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 export const gameRouter = createTRPCRouter({
   getAll: protectedProcedure.query(({ ctx }) => {
     return ctx.db.game.findMany({
-      where: { createdBy: { id: ctx.session.user.id } },
+      where: {
+        OR: [
+          { createdById: ctx.session.user.id },
+          {
+            participants: {
+              some: {
+                id: ctx.session.user.id,
+              }
+            }
+          }
+        ]
+      },
     });
   }),
 
-  create: protectedProcedure.input(z.object({ buyIn: z.number(), initialStack: z.number() })).mutation(({ ctx, input }) => {
+  create: protectedProcedure.input(z.object({
+    buyIn: z.number(),
+    initialStack: z.number(),
+    participants: z.array(z.object({
+      userId: z.string().optional(),
+      name: z.string(),
+      isRegisteredUser: z.boolean()
+    }))
+  })).mutation(({ ctx, input }) => {
     return ctx.db.game.create({
       data: {
         buyIn: input.buyIn,
         initialStack: input.initialStack,
-        participants: {},
         createdBy: { connect: { id: ctx.session.user.id } },
+        participants: {
+          create: input.participants.map(p => ({
+            user: p.userId ? { connect: { id: p.userId } } : undefined,
+            name: p.name,
+            isRegisteredUser: p.isRegisteredUser
+          })),
+        },
       },
     });
   }),
