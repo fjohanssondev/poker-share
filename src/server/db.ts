4e43c42248335fd21d5510eba63be79a6add31aa
Fredrik Johansson <fjohansson.creative@gmail.com>
2023-11-15 09:33:01 +0100

    chore: will be using torso later. Added adapter for it but uncommented

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,10 +1,19 @@
 import { PrismaClient } from "@prisma/client";
+import { createClient } from "@libsql/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
 
 import { env } from "~/env.mjs";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
+
+/* const libSQL = createClient({
+  url: `${process.env.TURSO_DATABASE_URL}`,
+  authToken: `${process.env.TURSO_AUTH_TOKEN}`
+});
+
+const adapter = new PrismaLibSQL(libSQL); */
 
 export const db =
   globalForPrisma.prisma ??
