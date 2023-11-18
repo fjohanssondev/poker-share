663274092c421f3b417175cbeaa1a634be90f746
Fredrik Johansson <fjohansson.creative@gmail.com>
2023-11-18 15:03:42 +0100

    feat: updated the page with input fields for each player. This is meant to be filled with whatever stack they ended with

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -1,7 +1,10 @@
 "use client"
 
+import type { Participant } from "@prisma/client";
 import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 import Timer from "~/app/_components/timer";
+import { stateOfTheGame } from "~/lib/state-of-the-game";
 import { api } from "~/trpc/react";
 
 export default function Game() {
@@ -9,6 +12,14 @@ export default function Game() {
   const { id } = useParams<{ id: string }>();
   const { data: game, isLoading } = api.game.getGameInfo.useQuery(id);
   const utils = api.useUtils()
+
+  const [players, setPlayers] = useState<Participant[]>([])
+
+  useEffect(() => {
+    if (game) {
+      setPlayers(game.participants)
+    }
+  }, [game])
 
   const startGame = api.game.startGame.useMutation({
     onSuccess: async () => {
@@ -21,41 +32,52 @@ export default function Game() {
     }
   });
 
+  // TODO: Use the loading.tsx route instead. Quick fix for now.
   if (isLoading) {
     return <p>Loading...</p>
   }
 
+  // Destructure the game object
   const { createdAt, initialStack, buyIn, startTime, isFinished, createdBy } = game ?? {};
 
   const handleStartGame = (id: string) => startGame.mutate(id)
-
-  const handleEndGame = (id: string) => endGame.mutate(id)
-
-  const stateOfTheGame = () => {
-    if (!isFinished && !startTime) {
-      return <span className="bg-zinc-200 text-black px-2 py-2 text-xs rounded-sm font-semibold">Game has not started yet</span>
-    } else if (!isFinished && startTime) {
-      return <span className="bg-amber-500 text-black px-2 py-2 text-xs rounded-sm font-semibold">Game is ongoing</span>
-    } else {
-      return <span className="bg-emerald-300 text-black px-2 py-2 text-xs rounded-sm font-semibold">The game has finished</span>
+  const handleEndGame = (id: string) => {
+    const checkStackAddsUp = players.reduce((acc, curr) => acc + Number(curr.stack), 0)
+    if (checkStackAddsUp !== initialStack! * players.length) {
+      return alert("The stacks don't add up to the initial stack")
     }
+    endGame.mutate(id)
   }
+  const gameState = stateOfTheGame(isFinished, startTime)
 
   return (
     <section>
       <h1 className="text-5xl text-secondary font-semibold">Game</h1>
       <div className="flex flex-col lg:flex-row gap-8 lg:gap-0">
-        <div className="flex-1">
-          <div className="flex items-center mt-8">
+        <div className="flex-1 order-2 lg:order-first">
+          <div className="flex items-center mt-2 lg:mt-8">
             <h2 className="text-3xl font-medium">Game Info</h2>
-            <span className="ml-8">{stateOfTheGame()}</span>
+            <span className="ml-8">{gameState}</span>
           </div>
-          <div className="mt-4">
-            <p>The game was created on {createdAt?.toLocaleDateString()}</p>
-            <p>Each player started with: <strong>{initialStack}</strong> chips</p>
-            <p>Each player bought in with: <strong>{buyIn}</strong></p>
-            <p>The game was created by: <strong>{createdBy?.name}</strong></p>
-          </div>
+          <ul className="mt-4">
+            <li>The game was created on {createdAt?.toLocaleDateString()}</li>
+            <li>Each player started with: <strong>{initialStack}</strong> chips</li>
+            <li>Each player bought in with: <strong>{buyIn}</strong></li>
+            <li>The game was created by: <strong>{createdBy?.name}</strong></li>
+          </ul>
+          {startTime && !isFinished && (
+            <>
+              <h2 className="text-3xl font-medium mt-8">Players</h2>
+              <ul className="flex flex-col gap-4 mt-4">
+                {players.map((participant) => (
+                  <li key={participant.id}>
+                    <label className="flex items-center mb-2">{participant.name}</label>
+                      <input onChange={(e) => setPlayers(prev => prev.map(player => player.userId === participant.userId ? { ...player, stack: Number(e.target.value) } : player))} type="number" className="rounded-sm w-2/3 bg-white/10 outline-offset-2 px-4 py-3 font-regular shadow-sm no-underline hover:bg-white/20" placeholder={`Enter the amount ${participant.name} ended with`} />
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
           {!startTime && !isFinished && (
             <button disabled={startGame.isLoading} onClick={() => handleStartGame(id)} className="rounded-sm w-full lg:w-2/3 mt-8 bg-secondary outline-offset-2 text-black px-10 py-3 shadow-sm font-semibold no-underline hover:bg-secondary/80" type="submit">
               Start game
@@ -68,7 +90,7 @@ export default function Game() {
           )}
         </div>
         {(startTime && !isFinished) && (
-          <div>
+          <div className="mt-8 lg:mt-0">
             <Timer gameId={id} />
           </div>
         )}
