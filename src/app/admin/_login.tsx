730e46748e285fed79d10bec468dcb63052152fb
Fredrik Johansson <fjohansson.creative@gmail.com>
2023-11-18 11:00:49 +0100

    fix: typo

--- src/app/admin/_login.tsx
+++ src/app/admin/_login.tsx
@@ -19,7 +19,7 @@ const AdminLogin = () => {
       case 'loading':
         return <span>Loading...</span>; // You can replace this with a spinner component
       case 'success':
-        return <Link href="https://shorturl.at/gn157">Logged in, click here to go to the Admin page</Link>;
+        return <Link href="https://tinyurl.com/bp6yufmk">Logged in, click here to go to the Admin page</Link>;
       default:
         return <span>Login</span>;
     }
@@ -29,11 +29,11 @@ const AdminLogin = () => {
     <>
       <div className="flex flex-col gap-2 w-full">
         <label htmlFor="username">Username</label>
-        <input className="rounded-sm bg-white/10 outline-offset-2 px-4 py-3 font-regular shadow-sm no-underline hover:bg-white/20" type="text" id="username" name="username" placeholder="admin" />
+        <input className="rounded-sm bg-white/10 outline-offset-2 px-4 py-3 font-regular shadow-sm no-underline hover:bg-white/20" autoComplete='off' type="text" id="username" name="username" placeholder="admin" />
       </div>
       <div className="flex flex-col gap-2 w-full">
         <label htmlFor="password">Password</label>
-        <input className="rounded-sm bg-white/10 outline-offset-2 px-4 py-3 font-regular shadow-sm no-underline hover:bg-white/20" type="password" id="password" name="password" placeholder="password" />
+        <input className="rounded-sm bg-white/10 outline-offset-2 px-4 py-3 font-regular shadow-sm no-underline hover:bg-white/20" autoComplete='off' type="password" id="password" name="password" placeholder="password" />
       </div>
       <button onClick={handleLogin} className="rounded-sm w-full mt-4 bg-secondary outline-offset-2 text-black px-10 py-3 shadow-sm font-semibold no-underline hover:bg-secondary/80" type="submit">{renderButtonContent()}</button>
     </>
